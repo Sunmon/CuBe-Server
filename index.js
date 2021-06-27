6180667964cf0637d95d6d6cd4595427caf8ef86
sunmon <blackmilk274@gmail.com>
2021-06-27 23:42:15 +0900

    [feat] 서버에서 프론트 public 폴더 정적으로 제공

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ const path = require('path');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('front', process.env.FRONT || path.join(__dirname, '../frontend')); // TODO: env 파일 설정
-/* its MIME type ('text/html') is not a supported ~ 에러 발생 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(app.get('front'), '/index.html')),
-); */
-app.use(
-  '/',
-  express.static(path.join(app.get('front'), '/')),
-); /* 에러 발생하지 않으나 모든 파일에 접근 가능 */
+app.use('/', express.static(path.join(app.get('front'), '/public')));
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), 'is listening...');
